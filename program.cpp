1d7842fc2c863164cc9f742c0a73c3107a67a65b
anhtuanng04050405 <anhtuanng04050405@gmail.com>
2026-09-24 15:49:26 +0700

    Them ham TinhHieu

--- program.cpp
+++ program.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
 
+int TinhHieu(int a, int b) {
+    return a - b;
+}
+
 int main() {
     cout << "Chuong trinh tinh toan nhom" << endl;
+    cout << "Hieu 10 - 4 = " << TinhHieu(10, 4) << endl;
     return 0;
 }
