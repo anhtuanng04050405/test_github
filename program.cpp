b2ca3c18213af04469a062db9218f4e5bccd686e
anhtuanng04050405 <anhtuanng04050405@gmail.com>
2026-09-24 15:47:09 +0700

    Them ham TinhTong

--- program.cpp
+++ program.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
 
+int TinhTong(int a, int b) {
+    return a + b;
+}
+
 int main() {
     cout << "Chuong trinh tinh toan nhom" << endl;
+    cout << "Tong 5 + 3 = " << TinhTong(5, 3) << endl;
     return 0;
 }
